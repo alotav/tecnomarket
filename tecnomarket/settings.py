1016c60bdc44756592e6bfc89334f6b7fcb1ac75
alotav <alonsotaverna@hotmail.com>
2022-12-16 16:48:14 -0300

    agrego login, logout

--- tecnomarket/settings.py
+++ tecnomarket/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 # MESSAGE_STORAGE = "django.contrib.messages.storage.cookie.CookieStorage"
 MESSAGE_STORAGE= "django.contrib.messages.storage.cookie.CookieStorage"
 
+LOGIN_REDIRECT_URL='/'
+LOGOUT_REDIRECT_URL='/'
 
 
 # Application definition
